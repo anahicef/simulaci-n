ee726cfdcc27ecddf50259ef8db82c712be69704
Anahí Esquivias <anahicef@hotmail.com>
2017-01-24 16:50:22 -0600

    modificación con comando for

--- histograma.m
+++ histograma.m
@@ -1,15 +1,14 @@
-l=5; 
-n1=3; %párticulas
-n2=3; %dimensiones
-d=round(unifrnd(-l,l,n1,n2));
+l=9; %largo de eje
+p=3; %atenuación
+n1=3; 
+n2=3;
+sumatoria=0;
+d=round(unifrnd(-l,l,n1,n2)); 
 d
-A=d(1,:); % esto se cambiaría por un for
-a=A.^2;
-AA=sum(a) 
-B=d(2,:);
-b=B.^2;
-BB=sum(b)
-C=d(2,:);
-c=C.^2;
-CC=sum(c) 
-suma=sqrt(AA+BB+CC); %distancia al origen
+for i = 1:n1
+  Z=d(i,:);
+  z=Z.^2;
+  s=sum(z);
+  sumatoria=s+sumatoria;
+endfor
+R=sqrt(sumatoria)
