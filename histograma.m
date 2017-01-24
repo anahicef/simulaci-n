4811489f1c899a7d36f5792647bcbacca36941f5
Anahí Esquivias <anahicef@hotmail.com>
2017-01-24 14:38:01 -0600

    avance de la clase 24/ene/2017

--- histograma.m
+++ histograma.m
@@ -3,7 +3,7 @@ n1=3; %párticulas
 n2=3; %dimensiones
 d=round(unifrnd(-l,l,n1,n2));
 d
-A=d(1,:); %esto se cambiaría por un for
+A=d(1,:); % esto se cambiaría por un for
 a=A.^2;
 AA=sum(a) 
 B=d(2,:);
@@ -12,4 +12,4 @@ BB=sum(b)
 C=d(2,:);
 c=C.^2;
 CC=sum(c) 
-suma=sqrt(AA+BB+CC) %distancia al origen
+suma=sqrt(AA+BB+CC); %distancia al origen
