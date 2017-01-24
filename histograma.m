0d0ed6d758d53d2cbed20b8eadeb05d556ed78a8
Anahí Esquivias <anahicef@hotmail.com>
2017-01-24 14:35:32 -0600

    Create histograma.m

---
+++ histograma.m
@@ -0,0 +1,15 @@
+l=5; 
+n1=3; %párticulas
+n2=3; %dimensiones
+d=round(unifrnd(-l,l,n1,n2));
+d
+A=d(1,:); %esto se cambiaría por un for
+a=A.^2;
+AA=sum(a) 
+B=d(2,:);
+b=B.^2;
+BB=sum(b)
+C=d(2,:);
+c=C.^2;
+CC=sum(c) 
+suma=sqrt(AA+BB+CC) %distancia al origen
