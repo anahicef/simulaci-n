9d35eff5f233d046ea258d621d39ee2db56df95e
Anahí Esquivias <anahicef@hotmail.com>
2017-01-31 13:34:40 -0600

    Practica 1

--- histograma.m
+++ histograma.m
@@ -2,27 +2,25 @@ clc
 %Definir constantes
 l=10;                %largo de eje
 p=2;                 %atenuación
-n1=3;                %No particulas
+n1=300;                %No particulas
 n2=3;                %No de dimensiones 
 sumatoria=0;
 
 %Inicio de programa
-V=(l/p)+1;           %valores permitidos
+V=((2*l)/p)+1;           %valores permitidos
 d=round(unifrnd(1,V,n1,n2)); 
 for i=1:n1
   for j=1:n2
-    d(i,j)=((d(i,j)+1)*p)-l;
-  endfor
+    d(i,j)=((d(i,j)-1)*p)-l;
+ endfor
 endfor
-d                    %Muestra matriz generada
+%d                 %Muestra matriz generada
 
-for i = 1:n2         %Operacioenes de distancia
+for i=1:n1        %Operaciones de distancia
   Z=d(i,:);
   z=Z.^2;
   s=sum(z);
-  sumatoria=s+sumatoria;
-endfor
-
-R=sqrt(sumatoria)
-
-hist(d)
+  R=sqrt(s);
+  r(1,i)=R;
+  endfor
+hist (r)
