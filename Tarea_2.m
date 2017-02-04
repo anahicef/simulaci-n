dedcc61a06f0d00287695e50e379a4fe90e472fe
Anahí Esquivias <anahicef@hotmail.com>
2017-02-03 18:48:35 -0600

    movimiento de particula en 3 dimensiones

--- Tarea_2.m
+++ Tarea_2.m
@@ -1,18 +1,22 @@
 clc
 #Definicion de variables
-t=10;        #tiempo
-l=5;         #largo de eje
+t=5;        #tiempo
+l=5;        #largo de eje
 a=0;
 
 #inicio de programa
-No=round(unifrnd(-l,l,1,1))  #Genera un número alearorio dentro de "l"
+N1=round(unifrnd(-l,l,1,1));  #Genera un número alearorio dentro de "l"
+N2=round(unifrnd(-l,l,1,1));  #Genera un número alearorio dentro de "l"
+N3=round(unifrnd(-l,l,1,1));  #Genera un número alearorio dentro de "l"
+No=[N1 N2 N3]
 
 for i=1:t                    #Bucle de tiempo 
 
-#"A" genera un numero al azar entre -1 y 1
+#"A" y "B" generan numeros al azar entre -1 y 1
 #se redondea
 #se suma al numero aleatorio generado
   A=round(unifrnd(-1,1,1,1));
+  B=round(unifrnd(-1,1,1,1));
   a=sum(A);
   if a==1                    
     a=1;                     
@@ -22,13 +26,22 @@ for i=1:t                    #Bucle de tiempo
   No=No+a;
   
 #Condición ciclo: al llegar al valor de "l" regresa a "-l" y viceversa
-  if No>l                    
-    No=-l;                    
-  elseif No<-l               
-    No=l;
+  if N1>l                    
+    N1=-l;                    
+  elseif N1<-l               
+    N1=l;
+  elseif N2>l
+    N2=-l;
+  elseif N2<-l               
+    N2=l;
+  elseif N3>l
+    N3=-l;
+  elseif N2<-l               
+    N2=l;
    else
-    No=No;
+    N1=N1;
+    N2=N2;
+    N3=N3;
     endif 
   No
-  
 endfor
