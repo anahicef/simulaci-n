0f4838f6ae466afdb6e81e7aefee7f2658eeef4a
Anahí Esquivias <anahicef@hotmail.com>
2017-02-07 14:39:16 -0600

    Tarea 2

--- Tarea_2.m
+++ Tarea_2.m
@@ -1,56 +1,51 @@
 clc
 #Definicion de variables
-t=15;        #tiempo
+t=3;        #tiempo
 l=5;        #largo de eje
+p=5;        #No particulas 
+n=2;         #No de dimensiones
 a=0;
 
 #inicio de programa
-N1=round(unifrnd(-l,l,1,1));  #Genera un número alearorio dentro de "l"
-N2=round(unifrnd(-l,l,1,1));  #Genera un número alearorio dentro de "l"
-N3=round(unifrnd(-l,l,1,1));  #Genera un número alearorio dentro de "l"
-No=[N1 N2 N3]
+N1=round(unifrnd(-l,l,p,n))  #Genera un número alearorio dentro de "l"
 
-for i=1:t                    #Bucle de tiempo 
 
-#"A" y "B" generan numeros al azar entre -1 y 1
+for T=1:t                    #Bucle de tiempo 
+
+#"A" B" "C" generan numeros al azar entre -1 y 1
 #se redondea
 #se suma al numero aleatorio generado
+
+
+#Define en q dimension se hará la suma
+d=round(unifrnd(1,n,1,1));
+
+for i=1:p
   A=round(unifrnd(-1,1,1,1));
-  B=round(unifrnd(-1,1,1,1));
-  C=round(unifrnd(-1,1,1,1));
   a=sum(A);
-  b=sum(B);
   if a==1                    
     a=1;                     
-   elseif a<1                     
+   else                     
    a=-1; 
-   elseif b==1
-    b=1;
-   else
-    b=-1;  
   endif
-  N1=N1+a;
-  N2=N2+b;
-  N3=N3+a;
   
+ N1(i,d)=N1(i,d)+a; 
+
 #Condición ciclo: al llegar al valor de "l" regresa a "-l" y viceversa
-  if N1>l                    
-    N1=-l;                    
-  elseif N1<-l               
-    N1=l;
-  elseif N2>l                    
-    N2=-l;                    
-  elseif N2<-l               
-    N2=l;
-   elseif N3>l                    
-    N3=-l;                    
-  elseif N3<-l               
-    N3=l;
+  if  N1(i,d)>l; 
+   N1(i,d)=-l;
+  elseif N1(i,d)<-l; 
+   N1(i,d)=l;
   else
-    N1=N1;
-    N2=N2;
-    N3=N3;
-  endif 
+   N1=N1;
+  endif
+
+endfor
+N1
+ 
+  plot(N1(:,1),N1(:,2),'p',"markersize", 10)
+  xlim([-l,l]);
+  ylim([-l,l]);
+  saveas(gcf,strcat('ploty',num2str(T),'.png'));
   
-  No=[N1 N2 N3]
 endfor
