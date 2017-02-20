6560fd878cb26c7404dbbd980415c9cb645444f5
Anahí Esquivias <anahicef@hotmail.com>
2017-02-20 15:11:20 -0600

    cmp

--- Tarea_2.m
+++ Tarea_2.m
@@ -2,8 +2,8 @@ clc
 #Definicion de variables
 t=3;        #tiempo
 l=5;        #largo de eje
-p=5;        #No particulas 
-n=2;         #No de dimensiones
+p=2;        #No particulas 
+n=2;        #No de dimensiones
 a=0;
 
 #inicio de programa
@@ -12,38 +12,45 @@ N1=round(unifrnd(-l,l,p,n))  #Genera un número alearorio dentro de "l"
 
 for T=1:t                    #Bucle de tiempo 
 
-#"A" B" "C" generan numeros al azar entre -1 y 1
-#se redondea
-#se suma al numero aleatorio generado
-
-
-#Define en q dimension se hará la suma
+#Define en q dimensión se hará la suma
 d=round(unifrnd(1,n,1,1));
 
-for i=1:p
-  A=round(unifrnd(-1,1,1,1));
-  a=sum(A);
-  if a==1                    
-    a=1;                     
-   else                     
-   a=-1; 
-  endif
+  for i=1:p
   
- N1(i,d)=N1(i,d)+a; 
+    #"A" generan numeros al azar entre -1 y 1
+    #se redondea
+    #se suma al numero aleatorio generado
+    A=round(unifrnd(-1,1,1,1));
+    a=sum(A);
+    if a<0                    
+      a=1;                     
+    else                     
+    a=-1; 
+    endif
+  
+  N1(i,d)=N1(i,d)+a; 
 
-#Condición ciclo: al llegar al valor de "l" regresa a "-l" y viceversa
-  if  N1(i,d)>l; 
-   N1(i,d)=-l;
-  elseif N1(i,d)<-l; 
-   N1(i,d)=l;
-  else
-   N1=N1;
+  #Condición ciclo: al llegar al valor de "l" regresa a "-l" y viceversa
+   if  N1(i,d)>l; 
+    N1(i,d)=-l;
+   elseif N1(i,d)<-l; 
+    N1(i,d)=l;
+   else
+    N1=N1;
+   endif
+
+  endfor
+  
+  N1
+  
+  #Comparación
+  N2=N1;
+  
+  if N1=N2;
+    printf("son iguales");
   endif
-
-endfor
-N1
  
-  plot(N1(:,1),N1(:,2),'p',"markersize", 10)
+  plot(N1(:,1),N1(:,2),'pr',"markersize", 10)
   xlim([-l,l]);
   ylim([-l,l]);
   saveas(gcf,strcat('ploty',num2str(T),'.png'));
