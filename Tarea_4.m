1da364722e1f0d2bbe0a481047ee78747d808925
Anahí Esquivias <anahicef@hotmail.com>
2017-04-07 17:55:45 -0500

    Tarea 4. Muestra rebote de particulas

--- Tarea_4.m
+++ Tarea_4.m
@@ -1,28 +1,41 @@
+
 clear
 clc
 #Definicion de variables
-t = 80;        #tiempo
-l = 10;        #largo de eje
-n = 25;         #No particulas 
+t = 25;        #tiempo
+l = 30;        #largo de eje
+n = 45;        #No particulas 
 d = 2;         #No de dimensiones
-umbral = l/30;
+atn=20;        #atenuación
+umbral = l/atn;
+tam = 0;
+
+#Vectores de color
+AzCi = [18 150 155] ./ 255;
+AzMa = [1 17 181] ./ 255;
+Az = [25 150 155] ./ 255;
+
 
 #inicio de programa
 C = unifrnd(0,l,n,d);
 D = unifrnd(-l/30,l/30,n,d);
 x = 1;
 y = 2;
-plot(C(:,x), C(:,y), 'og', "markersize", 20);
+
+
+#Imprime matriz para generación de párticulas
+plot(C(:,x), C(:,y), 'ko', 'LineWidth', 2, 'MarkerEdgeColor', AzCi, 'MarkerFaceColor', AzMa, 'MarkerSize', 15);
 xlim([0,l]);
 ylim([0,l]);
 colisiones = 0;
 
-#Condición bucle, llega a "l" y retorna a "-l"
+#Generación de movimiento aleatorio
 for T=1:t
   pause(0.2)             
   C = C + D;
   for i = 1:n
     for j = 1:d
+      #Condición bucle si llega "l" cambiar a "0"
       if  C(i,j) > l  
         C(i,j) -= l; 
       elseif C(i,j) < 0;  
@@ -31,21 +44,34 @@ for T=1:t
     endfor 
   endfor 
   
-  #Condición donde estan las colisiones
+  choque=[];
+  
   for p1 = 1 : (n - 1)
     for p2 = (p1 + 1) : n 
-       #sí hay colision genera rebote
-       if sqrt(sum((C(p1, :) - C(p2, :)).^2)) <= umbral
-          colisiones++;
-          cambio = D(p1, :);
-          D(p1, :) = -D(p2, :);
-          D(p2, :) = -cambio;
-       endif
+
+      #Sí doble de distancia oclidiana igual a umbral 
+      #entonces frenar
+      if 2*sqrt(sum((C(p1, :) - C(p2, :)).^2)) <= umbral
+        tam++;
+        
+      endif
+      
+      #Sí distancia oclidiana = a umbral 
+      #entonces generar un rebote
+      if sqrt(sum((C(p1, :) - C(p2, :)).^2)) <= umbral
+        colisiones++; #Cuenta colisiones obtenidas
+        D(p1, :) = -D(p2, :);
+        D(p2, :) = -D(p1, :);
+      endif
     endfor
   endfor
-  plot(C(:,x), C(:,y), 'og',"markersize", 20); #imprime rebote de particula
+
+  #Imprime colisiones
+  plot(C(:,x), C(:,y), 'ko', 'LineWidth', 2, 'MarkerEdgeColor', AzCi, 'MarkerFaceColor', AzMa, 'MarkerSize', 15);
   xlim([0,l]);
   ylim([0,l]);
+  
 endfor
 
-colisiones
+colisiones #Imprime colisiones obtenidas
+tam
